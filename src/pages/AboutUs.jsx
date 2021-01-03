17ca5220829e105653cda1cd730663cdcdbf71dd
NesyElimelech <nesy15@gmail.com>
2021-01-03 17:19:27 +0200

    Add the FAQ section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 // page component
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
+import FAQSection from '../components/FAQSection';
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FAQSection />
     </>
   );
 };
