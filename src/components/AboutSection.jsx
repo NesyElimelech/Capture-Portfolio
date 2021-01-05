e7998c43e1399eaed03d830f211bfe6d339b4b58
NesyElimelech <nesy15@gmail.com>
2021-01-05 16:37:04 +0200

    Add page animations with motion-framer

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Wave from './Wave';
 // Import image
 import home1 from '../img/home1.png';
 // import Styles
 import { Layout, Description, Hide, Image } from '../styles/LayoutStyles';
 // Framer Motion
 import { motion } from 'framer-motion';
+import { titleAnim, fade, photoAnim } from '../animation';
 
 const AboutSection = () => {
   return (
@@ -12,26 +14,27 @@ const AboutSection = () => {
       <Description>
         <motion.div className="title">
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>Dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. we
           have professionals with amazing skills.
-        </p>
-        <button>Contact us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="Man with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="Man with a camera" />
       </Image>
+      <Wave />
     </Layout>
   );
 };
--- src/pages/OurWork.jsx
+++ src/pages/OurWork.jsx
@@ -7,7 +7,14 @@ import theracer from '../img/theracer-small.png';
 import goodtimes from '../img/goodtimes-small.png';
 // Animations
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import {
+  pageAnimation,
+  fade,
+  photoAnim,
+  lineAnim,
+  slider,
+  sliderContainer,
+} from '../animation';
 
 const OurWork = () => {
   return (
@@ -18,25 +25,41 @@ const OurWork = () => {
       exit="exit"
       style={{ background: '#fff' }}
     >
+      <motion.div variants={sliderContainer}>
+        <Frame1 variants={slider}></Frame1>
+        <Frame2 variants={slider}></Frame2>
+        <Frame3 variants={slider}></Frame3>
+        <Frame4 variants={slider}></Frame4>
+      </motion.div>
       <Movie>
-        <h2>The Athlete</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fade}>The Athlete</motion.h2>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/the-athlete">
-          <img src={athlete} alt="A Boxer" />
+          <Hide>
+            <motion.img variants={photoAnim} src={athlete} alt="A Boxer" />
+          </Hide>
         </Link>
       </Movie>
       <Movie>
-        <h2>The Racer</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fade}>The Racer</motion.h2>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/the-racer">
-          <img src={theracer} alt="A Woman next to a car" />
+          <Hide>
+            <motion.img
+              variants={photoAnim}
+              src={theracer}
+              alt="A Woman next to a car"
+            />
+          </Hide>
         </Link>
       </Movie>
       <Movie>
-        <h2>Good Times</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fade}>Good Times</motion.h2>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/good-times">
-          <img src={goodtimes} alt="A Couple" />
+          <Hide>
+            <motion.img variants={photoAnim} src={goodtimes} alt="A Couple" />
+          </Hide>
         </Link>
       </Movie>
     </Work>
@@ -56,7 +79,7 @@ const Movie = styled.div`
   }
   .line {
     height: 0.5rem;
-    background-color: #cccccc;
+    background-color: #23d997;
     margin-bottom: 3rem;
   }
   img {
@@ -64,6 +87,30 @@ const Movie = styled.div`
     height: 70vh;
     object-fit: cover;
   }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+// Frame animation
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background-color: #fffebf;
+  z-index: 5;
+`;
+const Frame2 = styled(Frame1)`
+  background-color: #ff8efb;
+`;
+const Frame3 = styled(Frame1)`
+  background-color: #8ed2ff;
+`;
+const Frame4 = styled(Frame1)`
+  background-color: #8effa0;
 `;
 
 export default OurWork;
